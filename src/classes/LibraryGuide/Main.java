c8147acba12e2e33efcbb9467011cf9ea256b528
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-02-05 02:23:12 +0300

    obj methods hw done

--- src/classes/LibraryGuide/Main.java
+++ src/classes/LibraryGuide/Main.java
@@ -6,12 +6,5 @@ public class Main {
         Author kay = new Author("Адам", "Кей");
         Book thisIsGoingToHurt = new Book(" \"Будет больно\" ", kay, 2017);
         Book javaTheCompleteReference = new Book(" \"Java.Полное руководство.11-е издание\" ", shildt, 2018);
-
-        System.out.println("thisIsGoingToHurt.getName() = " + thisIsGoingToHurt.getPublicationYear());
-        System.out.println("thisIsGoingToHurt.getName() = " + thisIsGoingToHurt.getName());
-        System.out.println("kay.getName() = " + kay.getName());
-        System.out.println("kay.getSurname() = " + kay.getSurname());
-        thisIsGoingToHurt.setPublicationYear(2022);
-        System.out.println("thisIsGoingToHurt.getPublicationYear() = " + thisIsGoingToHurt.getPublicationYear());
     }
 }
